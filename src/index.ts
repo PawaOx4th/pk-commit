13ffd9d5fb45cf95a5f93496888db8a43f9c4601
Kaen Pawanachai <pawaox4th@gmail.com>
2023-03-25 16:39:23 +0700

    feat: 🎸 custom qustion

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
 #!/usr/bin/env node
+import inquirer, { QuestionCollection } from "inquirer"
+import typeForCommit from "./lib/comitType.js"
+import searchList from "inquirer-search-list"
 
-console.log("🔥 :")
+inquirer.registerPrompt("search-list", searchList)
+
+const questions: QuestionCollection = [
+  {
+    type: "search-list",
+    name: "type",
+    message: "What type of commit is this?",
+    choices: [...typeForCommit],
+  },
+  {
+    type: "input",
+    name: "message",
+    message: `Enter your commit message:
+    >`,
+    transformer(inputMsg, answers, flags) {
+      return `${answers.type} : ${inputMsg}`
+    },
+  },
+  {
+    type: "confirm",
+    name: "confirm",
+    message: "confirm commit message:",
+  },
+]
+
+inquirer.prompt(questions).then((answers) => {
+  console.log(answers)
+})
---
+++ src/lib/comitType.ts
@@ -0,0 +1,52 @@
+const typeForCommit = [
+  {
+    name: "ref♻️:",
+    value: "ref♻️:",
+  },
+  {
+    value: "chore🤖:",
+    name: "chore🤖:",
+  },
+  {
+    name: "ci🎡:",
+    value: "ci🎡:",
+  },
+  {
+    name: "docs✏️:",
+    value: "docs✏️:",
+  },
+  {
+    name: "feat✨:",
+    value: "feat✨:",
+  },
+  {
+    name: "fix🐛:",
+    value: "fix🐛:",
+  },
+  {
+    name: "perf🚀:",
+    value: "perf🚀:",
+  },
+  {
+    name: "revert🔀:",
+    value: "revert🔀:",
+  },
+  {
+    name: "style💄:",
+    value: "style💄:",
+  },
+  {
+    name: "test🧪:",
+    value: "test🧪:",
+  },
+  {
+    name: "other🍻:",
+    value: "other🍻:",
+  },
+  {
+    name: "release✅🎉:",
+    value: "release✅🎉:",
+  },
+]
+
+export default typeForCommit
